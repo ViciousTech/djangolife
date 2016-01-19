032e08768a386500014e6758f6b1553d47a865fd
Krishnanunni <krishnanunnir97@gmil.com>
2016-01-19 11:25:04 +0530

    Added all the files to repository

---
+++ qa/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class questions(models.Model):
+    question=models.TextField(max_length=300)
+    asker_email=models.EmailField(blank=True)
+    description=models.TextField(max_length=1000)
+
+class anwsers(models.Model):
+    question=models.TextField(max_length=300)
+    anwserer_email=models.EmailField(blank=True)
+    anwser=models.TextField(max_length=10000)
